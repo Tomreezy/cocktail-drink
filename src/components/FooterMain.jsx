a3c7a879d232601c79d4f2b1b2a62b4ce7debf47
Thomas <scelogumede95@gmail.com>
2024-08-10 11:50:10 +0200

    added font- padding

--- src/components/FooterMain.jsx
+++ src/components/FooterMain.jsx
@@ -3,7 +3,7 @@
 
 const FooterMain = ()=>{
     return (
-        <footer className="bg-indigo-950 text-white text-center">
+        <footer className="bg-indigo-950 p-4 text-white text-center">
             <p>Est 2020</p>
         </footer>
     )
