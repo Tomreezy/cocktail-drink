c4c8e65df5a70ec88df084a4043cb1536bd1fede
Thomas <scelogumede95@gmail.com>
2024-08-12 11:22:40 +0200

    fixed all the skwish

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -5,18 +5,23 @@ import pic5 from "../assets/pic5.png"
 
 const WhoWeAre = () => {
   return (
-    <section className="relative h-[90vh]  ">
+    <section className="relative h-[90vh]   ">
         <img src={pic2} className="size-full object-cover opacity-50 " />
-        <article className="absolute left-1/2  top-1/2 w-[80vw] md:py-20 transform -translate-x-1/2 -translate-y-1/2 flex  p-10 ">
-            <div className="flex flex-col md:w-1/2 space-y-5">
+       
+       
+        <article className="absolute left-0  h-full top-0 w-full md:py-20  flex  p-5 ">
+            
+            <div className="md:w-1/2 relative w-full    ">
+            <div className="flex flex-col absolute w-full md:p-5 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-5">
                 <header className="text-indigo-950 text-5xl font-bold">Getting <span>To Know Us</span></header>
                 <p className="text-lg">Who are we? What makes us tick? Welcome to the world
                 of takealot.com where extraordinary people come together 
                 to do extraordinary things, bringing world-class online shopping
                 to the people of South Africa.</p>
-                <button className="bg-blue-500 text-white font-semibold p-4 rounded-md">
-                Learn Abou Our Journey
+                <button className="bg-blue-500 self-start text-white font-semibold p-4 rounded-md">
+                Learn About Our Journey
                 </button>
+            </div>
             </div>
 
             <div className="hidden md:flex  w-1/2">
@@ -26,17 +31,20 @@ const WhoWeAre = () => {
                     <img src={pic5} className=" object-cover"/>
                     </div>
 
-                    <div className="absolute left-0 top-0 size-[50%] overflow-hidden -translate-y-[30%] translate-x-[30%] border-8  border-indigo-950 border-solid ">
+                    <div className="absolute left-0 top-0 size-[50%] translate-x-1/2 -translate-y-[30%] border-8  border-indigo-950 border-solid ">
                     <img src={pic3} className=" object-cover size-full "/>
                     </div>
 
-                    <div className="absolute left-0 top-0 overflow-hidden size-[70%] translate-x-1/2 translate-y-[60%] border-8  border-indigo-950 border-solid">
+                    <div className="absolute left-0 top-0  size-[50%] translate-x-1/2 translate-y-[90%] border-8  border-indigo-950 border-solid">
                     <img src={pic4} className=" object-cover size-full "/>
                     </div>
                     
                 </div>
             </div>
         </article>
+
+
+
     </section>
   )
 }
