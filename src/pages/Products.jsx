94fb8b37cc8fa203cc68abb248951b7e47eeb055
Thomas <scelogumede95@gmail.com>
2024-08-12 17:15:45 +0200

    added prod

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,9 +3,31 @@ import ProductsList from '../components/ProductsList'
 
 const Products = () => {
   return (
-    <main className='max-w-5xl mx-auto'>
-      <div className='h-[20vh] w-full'>filter</div>
+    <main className='max-w-5xl mx-auto grid md:grid-cols-3 mt-10'>
+      <aside className='hidden md:flex  p-5'>
+      <div className='h-[20vh] w-full'>
+
+      <input
+        type="text"
+        
+        placeholder="Search items..."
+        style={{
+          padding: '8px',
+          marginBottom: '16px',
+          width: '100%',
+          boxSizing: 'border-box'
+        }}
+      />
+     
+
+      </div>
+      </aside>
+
+
+
+      <div className='col-start-2 col-end-4'>
       <ProductsList />
+      </div>
     </main>
   )
 }
